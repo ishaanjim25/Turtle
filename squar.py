2191b4720d58c90b8ce0eb4dc6e61af37f0564ee
ishaanjim25 <ishaanjim25@gmail.com>
2026-04-23 21:19:10 +0100

    Add files via upload

---
+++ squar.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.Screen().bgcolor('blue')
+square=turtle.Turtle()
+
+square.right(90)
+square.forward(120)
+
+square.right(90)
+square.forward(120)
+
+square.right(90)
+square.forward(120)
+
+square.right(90)
+square.forward(120)
+
+turtle.done()
